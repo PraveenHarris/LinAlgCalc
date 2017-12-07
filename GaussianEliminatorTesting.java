a9045057979ce9ee7d864393204b2897164307f3
Praveen Harris <praveenharris@yahoo.com>
2017-12-07 02:44:52 -0500

    Update GaussianEliminatorTesting.java

--- GaussianEliminatorTesting.java
+++ GaussianEliminatorTesting.java
@@ -1,4 +1,3 @@
-
 public class GaussianEliminatorTesting {
 
     public static void start() {
@@ -6,15 +5,14 @@ public class GaussianEliminatorTesting {
                 {1, 3, 3},
                 {1, 7, 8},
                 {1, 6, 8}
-
         };
         GaussianEliminator eliminator = new GaussianEliminator(myMatrix);
         eliminator.ref();
         eliminator.printMatrix();
-
     }
 
     public static void main(String[] args) {
         start();
     }
+    
 }
